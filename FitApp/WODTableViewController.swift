7f861b27d4a4ddbc4dd222c278e54fb89693b9d4
Siddharth Patel <sid@siddharths-macbook-pro.local>
2016-01-13 19:35:51 -0500

    Commit Jan 13

--- FitApp/WODTableViewController.swift
+++ FitApp/WODTableViewController.swift
@@ -16,21 +16,25 @@ class WODTableViewController:UIViewController, UITableViewDelegate, UITableViewD
     @IBOutlet weak var WodTableView: UITableView!
     
     var GirlArray = [String]()
-    var HeroArray = [String]()
-    var HybridArray = [String]()
-    var OpenArray = [String]()
     var GirlArrayDetails = [String]()
+    var HeroArray = [String]()
     var HeroArrayDetails = [String]()
+    var HybridArray = [String]()
+    var HybridArrayDetails = [String]()
+    var OpenArray = [String]()
+    var OpenArrayDetails = [String]()
+   
     
     
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-// Parse Query to populate Girls workout table cell
+        
+// Marks:- Parse Query to populate Girls workout table cell
         let query0 = PFQuery(className: "CF_WOD")
-        let girls = ["The New Girls", "The Benchmark Girls"]
-        let runkey = query0.orderByDescending("createdAt").whereKey("Workout_Type", containedIn: girls)
+        let girlsType = ["The New Girls", "The Benchmark Girls"]
+        let runkey = query0.orderByDescending("createdAt").whereKey("Workout_Type", containedIn: girlsType)
         //runkey.whereKeyExists("The New Girls")
         runkey.findObjectsInBackgroundWithBlock {
             (objects: [PFObject]?, error: NSError?)-> Void in
@@ -52,10 +56,10 @@ class WODTableViewController:UIViewController, UITableViewDelegate, UITableViewD
             self.do_table_refresh()
             }
         
-// marks:- Parse query to get detail text
+// marks:- Girls Parse query to get detail text
         
-        let query2 = PFQuery(className: "CF_WOD")
-        let subkey = query2.orderByDescending("createdAt").whereKey("Workout_Type", containedIn: girls)
+        let query1 = PFQuery(className: "CF_WOD")
+        let subkey = query1.orderByDescending("createdAt").whereKey("Workout_Type", containedIn: girlsType)
       //  let subkey = query2.whereKeyExists("Workout_Type")
         subkey.findObjectsInBackgroundWithBlock {
             (objects: [PFObject]?, error: NSError?)-> Void in
@@ -76,12 +80,11 @@ class WODTableViewController:UIViewController, UITableViewDelegate, UITableViewD
             sleep (3)
             self.do_table_refresh()
         }
+// marks: Parse query to get hero
         
-      // marks: Parse query to get hero 
-        
-        let query3 = PFQuery(className: "CF_WOD")
-        var herotype = ["Hero"]
-        let heroKey = query3.orderByDescending("createdAt").whereKey("Workout_Type", containedIn: herotype)
+        let query2 = PFQuery(className: "CF_WOD")
+        let heroType = ["Hero"]
+        let heroKey = query2.orderByDescending("createdAt").whereKey("Workout_Type", containedIn: heroType)
         heroKey.findObjectsInBackgroundWithBlock {
             (objects: [PFObject]?, error: NSError?)-> Void in
             if error == nil{
@@ -101,10 +104,10 @@ class WODTableViewController:UIViewController, UITableViewDelegate, UITableViewD
             sleep (3)
             self.do_table_refresh()
         }
-        //marks: - 
-        let query4 = PFQuery(className: "CF_WOD")
-        let heroDkey = query4.orderByDescending("createdAt").whereKey("Workout_Type", containedIn: herotype)
-        //  let subkey = query2.whereKeyExists("Workout_Type")
+//marks: - Hero Parse query to get detail text
+        
+        let query3 = PFQuery(className: "CF_WOD")
+        let heroDkey = query3.orderByDescending("createdAt").whereKey("Workout_Type", containedIn: heroType)
         heroDkey.findObjectsInBackgroundWithBlock {
             (objects: [PFObject]?, error: NSError?)-> Void in
             if error == nil{
@@ -124,8 +127,104 @@ class WODTableViewController:UIViewController, UITableViewDelegate, UITableViewD
             sleep (3)
             self.do_table_refresh()
         }
+//Marks:- Parse query to get Hybrid
+        
+        let query4 = PFQuery(className: "CF_WOD")
+        let hybridType = ["Hybrid"]
+        let hybridkey = query4.orderByDescending("createdAt").whereKey("Workout_Type", containedIn: hybridType)
+        hybridkey.findObjectsInBackgroundWithBlock {
+            (objects: [PFObject]?, error: NSError?)-> Void in
+            if error == nil{
+                if let objects = objects as [PFObject]?
+                {
+                    for object in objects
+                    {
+                        let load = object.objectForKey("WorkOut_Name") as! String
+                        self.HybridArray.append(load)
+                    }
+                }
+                else
+                {
+                    print ("error")
+                }
+            }
+            sleep (3)
+            self.do_table_refresh()
+        }
+        
+//marks: - Parse Query to get Hybrid Details
 
+        let query5 = PFQuery(className: "CF_WOD")
+        let hybridDkey = query5.orderByDescending("createdAt").whereKey("Workout_Type", containedIn: hybridType)
+        hybridDkey.findObjectsInBackgroundWithBlock {
+            (objects: [PFObject]?, error: NSError?)-> Void in
+            if error == nil{
+                if let objects = objects as [PFObject]?
+                {
+                    for object in objects
+                    {
+                        let load = object.objectForKey("Workout_Type") as! String
+                        self.HybridArrayDetails.append(load)
+                    }
+                }
+                else
+                {
+                    print ("error")
+                }
+            }
+            sleep (3)
+            self.do_table_refresh()
+        }
+//Marks: - Parse Query to get Opens
+        
+        let query6 = PFQuery(className: "CF_WOD")
+        let openType = ["Open"]
+        let openkey = query6.orderByDescending("createdAt").whereKey("Workout_Type", containedIn: openType)
+        openkey.findObjectsInBackgroundWithBlock {
+            (objects: [PFObject]?, error: NSError?)-> Void in
+            if error == nil{
+                if let objects = objects as [PFObject]?
+                {
+                    for object in objects
+                    {
+                        let load = object.objectForKey("WorkOut_Name") as! String
+                        self.OpenArray.append(load)
+                    }
+                }
+                else
+                {
+                    print ("error")
+                }
+            }
+            sleep (3)
+            self.do_table_refresh()
+        }
+//Marks: - Parse query to get Open Details
+        
+        let query7 = PFQuery(className: "CF_WOD")
+        let openDkey = query7.orderByDescending("createdAt").whereKey("Workout_Type", containedIn: openType)
+        openDkey.findObjectsInBackgroundWithBlock {
+            (objects: [PFObject]?, error: NSError?)-> Void in
+            if error == nil{
+                if let objects = objects as [PFObject]?
+                {
+                    for object in objects
+                    {
+                        let load = object.objectForKey("Workout_Type") as! String
+                        self.OpenArrayDetails.append(load)
+                    }
+                }
+                else
+                {
+                    print ("error")
+                }
+            }
+            sleep (3)
+            self.do_table_refresh()
+        }
 
+        
+        
     }
     func do_table_refresh()
     {
@@ -160,7 +259,7 @@ class WODTableViewController:UIViewController, UITableViewDelegate, UITableViewD
                 returnvaule = HybridArray.count
                 break
                 case 3:
-                    returnvaule = OpenArray.count
+                returnvaule = OpenArray.count
                 break
             default:
                 break
@@ -187,9 +286,11 @@ class WODTableViewController:UIViewController, UITableViewDelegate, UITableViewD
                 break
             case 2:
                 cell.textLabel?.text = HybridArray[indexPath.row]
+               cell.detailTextLabel?.text = HybridArrayDetails[indexPath.row]
                 break
             case 3:
                 cell.textLabel?.text = OpenArray[indexPath.row]
+                cell.detailTextLabel?.text = OpenArrayDetails[indexPath.row]
                 break
             default:
                 break
@@ -210,86 +311,4 @@ class WODTableViewController:UIViewController, UITableViewDelegate, UITableViewD
 }
 
 
-/**PFQueryTableViewController {
 
-    struct object {
-        var sectionName : String!
-        var sectionObjects: [String]!
-    }
-    
-    
-    
-    override func queryForTable() -> PFQuery {
-        let query = PFQuery(className: "CF_WOD")
-        if [self.objects].count == 0 {
-            query.cachePolicy = PFCachePolicy.CacheElseNetwork
-        }
-        query.whereKey("Workout_Type", equalTo: "The Benchmark Girls")
-       // query.whereKey("Workout_Type", equalTo: "The New Girls")
-        return query
-    }
-    
-    
-    
-    //Marks: Function for populating table
-    
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath, object: PFObject?) -> PFTableViewCell?{
-        
-        let cell =  tableView.dequeueReusableCellWithIdentifier("CellWod", forIndexPath: indexPath) as! WODTableViewCell
-        cell.titleLabel.text = object?.objectForKey("WorkOut_Name") as? String
-        
-        return cell
-    }
-    //Marks: Function for Height of Rows
-    
-    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        
-        if indexPath.row + 1 > self.objects?.count
-        {
-            return 44
-        }
-        else
-        {
-        let height = super.tableView(tableView, heightForRowAtIndexPath: indexPath)
-        return height
-        }
-    }
-    
-    //Marks: Function for load page for more WODs
-    
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        
-        if indexPath.row + 1 > self.objects?.count
-        {
-            self.loadNextPage()
-            tableView.deselectRowAtIndexPath(indexPath, animated: true)
-            
-        }
-        else
-        {
-            self.performSegueWithIdentifier("ShowWodDetails", sender: self)
-        }
-        
-        
-    }
-    
-    // Marks: Function to show number of section for table view
-    
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        
-        return 2
-    }
-    
-    // Marks: Function to show title of differnet section of table view
-    
-    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if section == 0{
-            return "The Benchmark girls"
-        }
-        else
-        {
-            return "The New Girls"
-        }
-    }
-
- }**/
